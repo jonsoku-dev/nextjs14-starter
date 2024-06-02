92970fec03faba23510afa2d7b39d2640bb0d93e
jongseok-lee <jonsoku.dev@gmail.com>
2024-06-02 11:33:41 +0900

    form

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,25 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page(
+  { id }: { id: string },
+) {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
